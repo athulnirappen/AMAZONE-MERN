1817c3e66946c737d122b96302e046f799aa62f3
Athul Thomas <athulthomas001798@gmail.com>
2023-01-08 10:24:25 -0800

    axios is created and proxy is created

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,26 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import data from '../data'
+// import data from '../data'
+import axios from "axios";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+      
+    };
+    fetchdata();
+  }, []);
   return (
     <div>
       <h1>Featured Product</h1>
         <div className='products'>
-        {
-            data.products.map(product => (<div className='product' key={product.slug}>
+        
+          {products.map(product => (
+            
+            <div className='product' key={product.slug}>
             <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-                </Link>
-              <div className='product-info'>
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                  </Link>
+              <img src={product.image} alt={product.name} />
+            </Link>
+            <div className='product-info'>
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
               <p><strong>${product.price}</strong></p>
               <button>Add to cart</button>
+            </div>
               </div>
-          </div>))
-          }
+            
+          ))}
+          
           </div>
     </div>
   )
