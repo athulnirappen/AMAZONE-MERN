cb5a1493da721aa5534b88768954573ec23d5392
Athul Thomas <athulthomas001798@gmail.com>
2023-01-10 13:07:30 -0800

    react context is created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,55 @@
 import { BrowserRouter,  Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import {LinkContainer} from "react-router-bootstrap"
 
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import { Link } from 'react-router-dom';
+import Badge from 'react-bootstrap/Badge';
+import { useContext } from 'react';
+import { Store } from './store';
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <BrowserRouter>
-    <div className='d-flex flex-column site-container' >
-        <header >
+      <div className="d-flex flex-column site-container">
+        <header>
           <Navbar bg="dark" variant="dark">
             <Container>
-              <LinkContainer to='/'>
+              <LinkContainer to="/">
                 <Navbar.Brand>Amazona</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
-      </header>
-        <main className='mt-3'>
+        </header>
+        <main className="mt-3">
           <Container>
-          <Routes>
-            <Route path='/product/:slug' element={<ProductScreen/>}/>
-            <Route path='/' element={<HomeScreen/>}/>
-          </Routes>
-        </Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
         <footer>
-          <div className='text-center'>
-            All rights reserved
-          </div>
+          <div className="text-center">All rights reserved</div>
         </footer>
       </div>
-      </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
